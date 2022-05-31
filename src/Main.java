b206b4b3f35f5a4af05631736d549c257d882ace
Keaton Hutto <keatonhutto8@gmail.com>
2022-05-31 15:09:22 -0500

    coord update and interface

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,48 @@ class Main
 {
 
 
-    public  static void main(String args[])
-    {
+    public  static void main(String args[]) {
         JFrame frame = new JFrame("Mouse-Mover");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
+        frame.setSize(600, 300);
+        JButton startButton = new JButton("Start");
+        frame.add(startButton, BorderLayout.NORTH);
+        JButton stopButton = new JButton("Stop");
+        frame.add(stopButton, BorderLayout.SOUTH);
         frame.setVisible(true);
-        JTextArea jTextArea = new JTextArea();
+        //when start button is clicked, start the timer
+        startButton.addActionListener(e -> {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    int cursorPositionX = MouseInfo.getPointerInfo().getLocation().x;
+                    int cursorPositionY = MouseInfo.getPointerInfo().getLocation().y;
+                    int firstX = cursorPositionX + 1;
+                    int firstY = cursorPositionY + 1;
 
-        frame.add(jTextArea);
+                    try {
+                        Robot robot = new Robot();
+                        robot.mouseMove(firstX, firstY);
 
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                int cursorPositionX = MouseInfo.getPointerInfo().getLocation().x;
-                int cursorPositionY = MouseInfo.getPointerInfo().getLocation().y;
-                int firstX = cursorPositionX + 1;
-                int firstY = cursorPositionY + 1;
-
-                try {
-                    Robot robot = new Robot();
-                    robot.mouseMove(firstX, firstY);
-
-
-                }catch (AWTException e) {
-                    e.printStackTrace();
+                    } catch (AWTException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Running: " + new java.util.Date());
+                    JTextArea jTextArea = new JTextArea();
+                    jTextArea.setEditable(false);
+                    jTextArea.setText("Running: " + "X Coordinate: "+firstX + " " + "Y Coordinate: " + firstY + " " + new java.util.Date());
+                    frame.add(jTextArea, BorderLayout.CENTER);
+                    frame.add(jTextArea);
+                    frame.setVisible(true);
+                    stopButton.addActionListener(e -> timer.cancel());
+                    //system.out.print size of jframe
+                    System.out.println(frame.getSize());
                 }
-                System.out.println("Running: " + new java.util.Date());
-            }
-        }, 0, 30000);}}
+            }, 0, 10000);
+        });}}
+
+
+
+
 
